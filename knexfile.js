0fe1474ee1a007286fefe6ca01c27d65152865ca
Rafael Batista <rafaabatistas@gmail.com>
2021-05-07 12:48:53 -0300

    Change knexfile config

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,26 @@
 const user = process.env.USER_ENV;
 const pass = process.env.PASS_ENV;
+const hostRemoto = process.env.HOST_REMOTO;
+const userRemoto = process.env.USER_REMOTO;
+const passRemoto = process.env.PASS_REMOTO;
+const databaseRemoto = process.env.DATABASE_REMOTO;
 
 module.exports = {
-  production: {
+  development: {
     client: 'pg',
     connection: {
       database: 'bd_ibira',
       user: user,
       password: pass,
-    },
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: hostRemoto,
+      database: databaseRemoto,
+      user: userRemoto,
+      password: passRemoto,
+    }
   }
 };
